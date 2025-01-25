de73d56c6f905c263f6a9f628f032b642c6efce6
Kaniteru <80136676+kaniteru@users.noreply.github.com>
2025-01-25 16:23:23 +0900

    added OrderedThreadPool
    - removed ThreadPool::Config
    - updated constructor of ThreadPool
    - added OrderedThreadPool

--- include/kani/thread_pool.hpp
+++ include/kani/thread_pool.hpp
@@ -8,11 +8,9 @@
 #include <condition_variable>
 
 /* thread_pool.hpp
- * Included classes:
+ *  Included classes:
  *      - kani::ThreadPool
- *
- * Included structs:
- *      - kani::ThreadPool::Config
+ *      - kani::OrderedThreadPool
  */
 
 namespace kani {
@@ -24,14 +22,6 @@ namespace kani {
 class ThreadPool {
 public:
     using worker_task_t = std::function<void()>;
-
-    struct Config {
-        /* Sets the number of threads for the workers.
-             You can check the maximum number of workers that can be used with 'ThreadPool::MAX_WORKER_THREADS'. */
-        size_t m_numThreads;
-        /* If true, m_numThreads is fixed to 1, and no other tasks will be executed until the current task is completed. */
-        bool    m_orderedTask;
-    };
 
     /**
      * @return Returns true if worker running.
@@ -102,7 +92,7 @@ public:
      * @return Returns true if workers stopped successfully.
      */
     bool stop();
-private:
+protected:
     /**
      * @brief A worker thread.
      */
@@ -110,21 +100,13 @@ private:
 
 public:
     /**
-     * @brief Constructor.<br>
-     * Uses the maximum available workers.
+     * @param [in] lenWorkers Specifies the number of worker threads to use.
+     * You can check the maximum available worker threads with ThreadPool::MAX_WORKER_THREADS.
      */
-    ThreadPool();
-
-    /**
-     * @brief Constructor.<br>
-     * Initializes the thread pool according to the provided config.
-     *
-     * @param [in] config Config for the thread pool.
-     */
-    explicit ThreadPool(const ThreadPool::Config& config);
+    explicit ThreadPool(size_t lenWorkers = ThreadPool::MAX_WORKER_THREADS);
     ~ThreadPool();
-private:
-    const size_t m_lenThreads;
+protected:
+    const size_t m_lenWorkers;
     const bool m_orderedTask;
     std::atomic_bool m_running;
     std::atomic_bool m_requestedStop;
@@ -132,7 +114,6 @@ private:
     std::queue<worker_task_t> m_tasks;
     std::condition_variable m_cv;
     mutable std::mutex m_mtx;
-    mutable std::mutex m_orderedMtx;
 public:
     static const size_t MAX_WORKER_THREADS; /* The maximum number of workers that can be created. */
 };
@@ -188,7 +169,7 @@ bool ThreadPool::start() {
     m_running = true;
     m_requestedStop = false;
 
-    for (size_t i = 0; i < m_lenThreads; i++) {
+    for (size_t i = 0; i < m_lenWorkers; i++) {
         std::thread thread(&ThreadPool::worker_thread, this);
         m_workers.emplace_back(std::move(thread));
     }
@@ -239,27 +220,14 @@ void ThreadPool::worker_thread() {
             m_tasks.pop();
         }
 
-        if (m_orderedTask) {
-            std::unique_lock<std::mutex> lock(m_orderedMtx);
-            task();
-        }
-        else {
-            task();
-        }
+        task();
     }
 }
 
 inline
-ThreadPool::ThreadPool() :
-    m_lenThreads(ThreadPool::MAX_WORKER_THREADS),
+ThreadPool::ThreadPool(const size_t lenWorkers) :
+    m_lenWorkers(lenWorkers),
     m_orderedTask(false),
-    m_running(false),
-    m_requestedStop(true) { }
-
-inline
-ThreadPool::ThreadPool(const ThreadPool::Config& config) :
-    m_lenThreads(config.m_orderedTask ? 1 : config.m_numThreads),
-    m_orderedTask(config.m_orderedTask),
     m_running(false),
     m_requestedStop(true) { }
 
@@ -269,6 +237,80 @@ ThreadPool::~ThreadPool() {
 }
 
 const size_t ThreadPool::MAX_WORKER_THREADS = std::thread::hardware_concurrency();
+
+// ======================== C L A S S ========================
+// ===    kani::OrderedThreadPool
+// ======================== C L A S S ========================
+
+class OrderedThreadPool : public ThreadPool {
+public:
+    /**
+     * @return Returns true if worker started successfully.
+     */
+    bool start();
+protected:
+    /**
+     * @brief A worker thread.
+     */
+    void worker_thread();
+
+public:
+    /**
+     * @brief The number of worker threads is fixed at 1, and no other tasks will proceed until the current task is completed.
+     */
+    OrderedThreadPool();
+    ~OrderedThreadPool() = default;
+protected:
+    mutable std::mutex m_orderedMtx;
+};
+
+// ======================== C L A S S ========================
+// ===    kani::OrderedThreadPool
+// ======================== C L A S S ========================
+
+inline
+bool OrderedThreadPool::start() {
+    if (m_running) {
+        return false;
+    }
+
+    std::unique_lock<std::mutex> lock(m_mtx);
+
+    m_running = true;
+    m_requestedStop = false;
+
+    std::thread thread(&OrderedThreadPool::worker_thread, this);
+    m_workers.emplace_back(std::move(thread));
+    return true;
+}
+
+inline
+void OrderedThreadPool::worker_thread() {
+    while (true) {
+        worker_task_t task { };
+
+        {
+            std::unique_lock<std::mutex> lock(m_mtx);
+            m_cv.wait(lock, [&] {
+                return !m_tasks.empty() || m_requestedStop;
+            });
+
+            if (m_requestedStop) {
+                return;
+            }
+
+            task = std::move(m_tasks.front());
+            m_tasks.pop();
+        }
+
+        std::unique_lock<std::mutex> lock(m_orderedMtx);
+        task();
+    }
+}
+
+inline
+OrderedThreadPool::OrderedThreadPool() :
+    ThreadPool(1) { }
 } //namespace kani
 
 
